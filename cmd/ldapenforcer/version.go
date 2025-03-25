ac11539314341c790348508fdb3e8278300e9eb9
Micah R Ledbetter <me@micahrl.com>
2025-03-24 19:14:39 -0500

    Add version --raw

--- cmd/ldapenforcer/version.go
+++ cmd/ldapenforcer/version.go
@@ -7,16 +7,24 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// flagRaw determines whether to output just the version string (for scripting usage)
+var flagRaw bool
+
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of LDAPEnforcer",
 	Long:  `All software has versions. This is LDAPEnforcer's.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("LDAPEnforcer version %s\n", version.GetVersion())
+		if flagRaw {
+			fmt.Println(version.GetVersion())
+		} else {
+			fmt.Printf("LDAPEnforcer version %s\n", version.GetVersion())
+		}
 	},
 }
 
 func init() {
+	versionCmd.Flags().BoolVarP(&flagRaw, "raw", "r", false, "Print only the raw version number")
 	RootCmd.AddCommand(versionCmd)
 }
